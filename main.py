e8881f61b09a74f4c8ef1007eae6620d6ed3b19d
Jelle Bouwman <32744870+jellebouwman@users.noreply.github.com>
2024-06-03 14:24:35 +0200

    Add 'print_report' function that outputs analysis summary to the terminal

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ def main():
 	book_path = "books/frankenstein.txt"
 	text = get_book_text(book_path)
 	number_of_words = get_number_of_words(text)
-	print(f"{number_of_words} words found in the document")
 	character_dict_count = get_character_count_dict(text)
-	print(f"The character count dictionary is: {character_dict_count}")
+	sorted_character_list = sort_character_dict(character_dict_count)
+
+	print_report(book_path, number_of_words, sorted_character_list)
 
 def get_book_text(path):
 	with open(path) as f:
@@ -23,5 +24,32 @@ def get_character_count_dict(text):
 		else:
 			character_count_dict[char] = 1
 	return character_count_dict
+
+# Refactors a dict of character keys and count values to a sorted
+# list of dicts that contain keys for the character and counts
+# also filters out non-alphabetic characters
+def sort_character_dict(character_dict):
+	character_count_list = []
+
+	def sort_on(dict):
+		return dict["count"]
+
+	for char, count in character_dict.items():
+		if char.isalpha():
+			character_count_list.append({"char": char, "count": count})
+
+	character_count_list.sort(reverse=True, key=sort_on)
+
+	return character_count_list
+
+def print_report(book_path, word_count, sorted_char_dict):
+	print(f"--- Begin report of {book_path} ---")
+	print(f"{word_count} words found in the document \n")
+
+	for char in sorted_char_dict:
+		print(f"The '{char['char']}' character was found {char['count']} times")
+
+	print(f"--- End report of {book_path} ---")
+
 
 main()
